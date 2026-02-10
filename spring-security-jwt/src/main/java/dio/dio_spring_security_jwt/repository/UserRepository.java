67e85003cb00a7284183f0dc5f96d2283c5d52ed
Thiago Renan <thiagorenan1108@gmail.com>
2026-02-09 23:25:34 -0300

    Adicionando o PostgreSQL ao projeto

--- spring-security-jwt/src/main/java/dio/dio_spring_security_jwt/repository/UserRepository.java
+++ spring-security-jwt/src/main/java/dio/dio_spring_security_jwt/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username = :username")
-    public User findByUsername(@Param("username") String username);
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.userName = :username")
+    public User findByUsername(@Param("userName") String username);
     }
 
